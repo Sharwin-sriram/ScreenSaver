59b751648e0e7361f7e2a3c4d09e7936bf9bb3a8
Sharwin-sriram <shreeramtooo@gmail.com>
2025-08-17 11:58:58 +0530

    added notes and globalised colors,fonts...etc

--- screensaver.py
+++ screensaver.py
@@ -6,6 +6,12 @@ from PIL import Image, ImageTk
 from io import BytesIO
 import ctypes
 
+
+#---------- NOTES -----------------
+# USE 6-DIGIT HEX FOR COLORS
+# REPLACE THE API KEY WITH YOUR OWN API KEY
+# GET A API KEY FROM https://openweathermap.org/
+
 run_screensaver = False
 preview_mode = False
 preview_hwnd = None
@@ -22,6 +28,17 @@ API_KEY = ""
 IPINFO_API = "http://ip-api.com/json/"
 LAT, LON, CITY, REGION = 0, 0, "", ""
 
+CLOCK_SIZE = 120        # MAIN CLOCK FONT SIZE
+FONT_FAMILY = "Calibri"         #FONT OF THE OVERALL GUI
+TEMP_FONT = (FONT_FAMILY, 32)       #Replace FONT_FAMILY HERE FOR DIFFERENT FONT FOR TEMPERATURE
+DATE_FONT = (FONT_FAMILY, 30)       #Replace FONT_FAMILY HERE FOR DIFFERENT FONT FOR DATE
+
+DATE_COLOR = "white"        #COLOR OF THE DATE ABOVE THE CLOCK    
+WEATHER_COLOR = "white"     #COLOR OF THE WEATHER UNDER THE CLOCK
+CLOCK_COLOR = "white"       #COLOR OF THE CLOCK
+AM_COLOR = "#a2a8ff"       #COLOR FOR AFTER MERIDIAN
+PM_COLOR = "#37c0f7"       #COLOR FOR POST MERIDIAN
+
 if len(sys.argv) > 1:
     arg = sys.argv[1].lower()
     if arg == "/s":
@@ -37,7 +54,7 @@ if len(sys.argv) > 1:
             except ValueError:
                 sys.exit()
 else:
-    run_screensaver = True
+    sys.exit()          # CHANGE THIS TO run_screensaver = True FOR TESTING
 if not run_screensaver:
     sys.exit()
     
@@ -46,6 +63,7 @@ if preview_mode and preview_hwnd:
     root.overrideredirect(True)
 else:
     root.attributes("-fullscreen", True)
+
 
 
 try:
@@ -79,25 +97,80 @@ def get_weather():
 
 def draw_clock_text():
     canvas.delete("clock")
-    now = time.strftime("%I:%M %p")
+    now = time.strftime("%I:%M")
+    ampm = time.strftime("%p")
     date = time.strftime("%a, %d %b")
+
     x = root.winfo_screenwidth() // 2
     y = root.winfo_screenheight() // 2
-    canvas.create_text(x, y - 90, text=date, fill="white", font=("Calibri", 30), tags="clock")
-    canvas.create_text(x, y, text=now, fill="white", font=("Calibri", 120), tags="clock")
-    if current_weather != "Weather N/A" and weather_icon:
-        temp_font = ("Calibri", 32)
-        text_width = canvas.create_text(0, 0, text=current_weather, font=temp_font, tags="measure")
+
+    canvas.create_text(
+        x, y - 90, 
+        text=date, 
+        fill=DATE_COLOR,
+        font=DATE_FONT, 
+        tags="clock"
+    )
+
+    time_id = canvas.create_text(0, 0, text=now, font=(FONT_FAMILY, CLOCK_SIZE), tags="measure")
+    ampm_id = canvas.create_text(0, 0, text=ampm, font=(FONT_FAMILY, CLOCK_SIZE), tags="measure")
+
+    bbox_time = canvas.bbox(time_id)
+    bbox_ampm = canvas.bbox(ampm_id)
+
+    time_w = bbox_time[2] - bbox_time[0] if bbox_time else 0
+    ampm_w = bbox_ampm[2] - bbox_ampm[0] if bbox_ampm else 0
+
+    canvas.delete("measure")
+
+    total_w = time_w + 20 + ampm_w
+
+    start_x = x - total_w // 2
+
+    canvas.create_text(
+        start_x, y,
+        text=now, 
+        fill=CLOCK_COLOR,
+        font=(FONT_FAMILY, CLOCK_SIZE),
+        anchor="w", tags="clock"
+    )
+    if ampm == "AM":
+        canvas.create_text(
+            start_x + time_w + 20, y,
+            text=ampm, 
+            fill=AM_COLOR,
+            font=(FONT_FAMILY, CLOCK_SIZE),
+            anchor="w", tags="clock"
+        )
+    elif ampm == "PM":
+        canvas.create_text(
+            start_x + time_w + 20, y,
+            text=ampm, 
+            fill=PM_COLOR,
+            font=(FONT_FAMILY, CLOCK_SIZE),
+            anchor="w", tags="clock"
+        )
+
+    if current_weather != "Weather N/A":
+        
+        text_width = canvas.create_text(0, 0, text=current_weather, font=TEMP_FONT, tags="measure")
         bbox = canvas.bbox(text_width)
         canvas.delete("measure")
         text_w = bbox[2] - bbox[0] if bbox else 0
         icon_w = weather_icon.width()
         total_w = icon_w + 10 + text_w
         start_x = x - total_w // 2
-        canvas.create_image(start_x + icon_w // 2, y + 110, image=weather_icon, tags="clock")
-        canvas.create_text(start_x + icon_w + 10, y + 110, text=current_weather, fill="white", font=temp_font, anchor="w", tags="clock")
+        canvas.create_image(start_x + icon_w // 2, y + 110,
+                            image=weather_icon,
+                            tags="clock")
+        canvas.create_text(start_x + icon_w + 10, y + 110, 
+                           text=current_weather, 
+                           fill=WEATHER_COLOR, 
+                           font=TEMP_FONT, 
+                           anchor="w", 
+                           tags="clock")
     else:
-        canvas.create_text(x, y + 90, text="Weather N/A", fill="white", font=("Calibri", 32, "bold"), tags="clock")
+        canvas.create_text(x, y + 90, text="Weather N/A", fill=WEATHER_COLOR, font=TEMP_FONT, tags="clock")
     root.after(1000, draw_clock_text)
 
 def update_weather():
